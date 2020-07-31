aa422044af4ae49308e972025facbaacd9c89daf
nobuyuki inoue <spring555@gmail.com>
2020-07-31 16:58:08 +0900

    modify prompts and ssh functions.

--- pyTelnetCmdExec.py
+++ pyTelnetCmdExec.py
@@ -35,7 +35,7 @@ def main():
     lines = read_cmdlist_file(argv[1])
 
     # Set of standby prompt characters
-    prompts = [b">$", b"> $", b"#$", b"# $", b"\\$$", b"\\$ $", b"assword: $", b"login: $", b"name: $"]
+    prompts = [b">$", b"> $", b"#$", b"# $", b"\\$$", b"\\$ $", b"%$", b"% $", b"[Pp]assword: $", b"login: $", b"name: $"]
 
     # Read connection information.
     cn = set_ConnectionInformation(lines, timeout=10)
@@ -105,6 +105,9 @@ def set_ConnectionInformation(lines, timeout):
     return cn
 
 def print_and_append(buffer, outputString):
+    """
+    print and append to list output string.
+    """
     print(outputString, end="")
     if buffer != None:
         buffer.append(outputString)
@@ -120,7 +123,6 @@ def connect_telnet(cn, prompts):
         return None
 
     if cn.username != "":
-
         # Wait for username prompt.
         response = tn.read_until(b': ', cn.timeout)
         print_and_append(responseLog, response.decode())
@@ -143,7 +145,6 @@ def connect_telnet(cn, prompts):
             return None, responseLog
 
         print_and_append(responseLog, response[2].decode())
-
     else:
         # Wait for password prompt.
         response = tn.read_until(b': ', cn.timeout)
@@ -152,18 +153,30 @@ def connect_telnet(cn, prompts):
     return tn, responseLog
 
 def detect_promptString(decoded_response):
-        pos0 = decoded_response.rfind("\n")
-        workLine = decoded_response[pos0 + 1:]
-        prompt_chars = [">", "#", "$"]
-        prompt_preStr = None
-        for ch in prompt_chars:
-            pos = workLine.find(ch)
-            if pos > 0:
-                prompt_preStr = workLine[:pos]
-                break
-        return prompt_preStr
+    """
+    detect prompt string.
+    """
+    if "MacBook" in decoded_response:
+        lines = decoded_response.split("\n")
+        for line in lines:
+            for fld in line.split(" "):
+                if "@" in fld:
+                    return fld
+    pos0 = decoded_response.rfind("\n")
+    workLine = decoded_response[pos0 + 1:]
+    prompt_chars = [">", "#", "$", "@"]
+    prompt_preStr = None
+    for ch in prompt_chars:
+        pos = workLine.find(ch)
+        if pos > 0:
+            prompt_preStr = workLine[:pos]
+            break
+    return prompt_preStr
 
 def set_output_filename(prompt_preStr, cn, log_path):
+    """
+    set log output filename.
+    """
     prompt_preStr = remove_prohibited_characters(prompt_preStr)
     dtStr = datetime.datetime.now().strftime('_%Y%m%d_%H%M%S')
     output_filename = log_path + prompt_preStr + "_" + cn.ipaddr + dtStr + ".log"
@@ -232,7 +245,12 @@ def cmdlist_exec_telnet(lines, cn, prompts, enable_log_output, log_path):
         tn.write(line.encode() + b"\n")
         count += 1
 
-        res = tn.expect(prompts, timeout=cn.timeout)
+        if prompt_preStr == None:
+            res = tn.expect(prompts, timeout=cn.timeout)
+        elif prompt_preStr_Regulars != None:
+            res = tn.expect(prompt_preStr_Regulars, timeout=cn.timeout)
+        else:
+            res = tn.expect(prompts, timeout=cn.timeout)
         response = res[2]
 
         # Convert byte to string.
@@ -250,6 +268,7 @@ def cmdlist_exec_telnet(lines, cn, prompts, enable_log_output, log_path):
                 for buf in responseLog:
                     wf.write(buf.replace("\n", ""))
                 responseLog = None
+                prompt_preStr_Regulars = [b"\n" + prompt_preStr.encode() + b".*$", b"[Pp]assword: "]
 
         # Write to stdout and file.
         print_and_write(decoded_response, wf, responseLog, enable_removeLF=True)
@@ -270,13 +289,13 @@ def cmdlist_exec_ssh(lines, cn, prompts, enable_log_output, log_path):
     """
 #   PROMPTS = [prompt_byte.decode() for prompt_byte in prompts]
 #   prompts = [".*>\s*", ".*#\s*", ".*\$\s*", ".*@.*\n\~.*", ".*assword:\s*"]
-    prompts = [".*>\s*", ".*#\s*", ".*\$\s*", ".*@.*", ".*assword:\s*"]
+    prompts = [".*>\s*", ".*#\s*", ".*\$\s*", ".*@.*", ".*%.*", ".*[Pp]assword:\s*"]
 
     # Start SSH connection
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.WarningPolicy())
     client.connect(cn.ipaddr, username = cn.username, password = cn.passwd)
-    interact = paramiko_expect.SSHClientInteraction(client, timeout = cn.timeout, display = False)
+    interact = paramiko_expect.SSHClientInteraction(client, buffer_size = 10*1024*1024, timeout = cn.timeout, display = False)
 
     index = interact.expect(prompts)
 #   decoded_response = interact.current_output_clean
@@ -289,7 +308,7 @@ def cmdlist_exec_ssh(lines, cn, prompts, enable_log_output, log_path):
     # prompt string detection
     wf = None
     prompt_preStr = detect_promptString(decoded_response)
-
+    prompt_preStr_Regulars = [prompt_preStr + ".*", "[Pp]assword:\s*"]
     if prompt_preStr != None and enable_log_output:
         # logfile open.
         wf = open(set_output_filename(prompt_preStr, cn, log_path), mode='wt')
@@ -312,27 +331,28 @@ def cmdlist_exec_ssh(lines, cn, prompts, enable_log_output, log_path):
             count += 1
             continue
 
-        """
-        interact.expect(prompts)
-        decoded_response = interact.current_output
-
-        # Write to stdout and file.
-        print_and_write(decoded_response, wf, responseLog, enable_removeLF=False)
-        """
-
         # command send.
+    #   print("<<send = {0}>>".format(line), end="")
         interact.send(line)
         count += 1
 
         try:
-            index = interact.expect(prompts)
-            decoded_response = interact.current_output
+            if prompt_preStr == None:
+            #   print("<<expext(prompts)>>")
+                index = interact.expect(prompts)
+            else:
+            #   print("<<expext(prompt_preStr_Regulars)>>")
+                index = interact.expect(prompt_preStr_Regulars)
         except:
-            continue
+        #   print("except occured.")
+            None
+
+    #   print("len(interact.current_output) = {0:d}".format(len(interact.current_output)))
+        decoded_response = interact.current_output
 
         # Write to stdout and file.
         print_and_write(decoded_response, wf, responseLog, enable_removeLF=False)
-        
+
     if interact != None:
         interact.close()
     
@@ -340,7 +360,6 @@ def cmdlist_exec_ssh(lines, cn, prompts, enable_log_output, log_path):
         wf.close()
 
     return
-
 
 if __name__ == '__main__':
     main()
